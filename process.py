f72727d95f1c047348738dc250f6bfa536ee1069
mcintyre1994 <mcintyre1994@gmail.com>
2018-01-21 20:07:52 +0000

    Use urls.cfg file for URLs

--- process.py
+++ process.py
@@ -7,29 +7,41 @@ import time
 
 sleep_time = 10
 
-def get_pending_hosts(read_url):
-    return requests.get(read_url).json()
 
 
-def process_pending_hosts(read_url, write_url):
+def get_pending_hosts(read_url, read_key):
+    print(read_url, read_key)
+    return requests.get(read_url, params={'code': read_key, 'subset': 'pending'}).json()
 
-    hosts = get_pending_hosts(read_url)
+
+def can_process_host(host):
+    print("Checking if we can process...")
+    print(host)
+    return 'host_version' in host and 'installed_updates' in host
+
+
+def process_pending_hosts(read_url, read_key, write_url, write_key):
+    hosts = [host for host in get_pending_hosts(read_url, read_key) if can_process_host(host)]
     print(hosts)
     for host in hosts:
         host = HostProcess.populate_available_updates(host)
         print(host)
-        requests.post(write_url, json=host)
+        requests.post(write_url, params={'code': write_key}, json=host)
 
 def main():
 
-    config = configparser.ConfigParser()
-    config.read('urls.cfg')
+    urls_config = configparser.SafeConfigParser()
+    urls_config.read('urls.cfg')
+    read_url = urls_config.get('urls', 'read_url')
+    write_url = urls_config.get('urls', 'write_url')
 
-    read_url = config.get('urls', 'read_url')
-    write_url = config.get('urls', 'write_url')
+    credentials_config = configparser.SafeConfigParser()
+    credentials_config.read('credentials.cfg')
+    read_key = credentials_config.get('apis', 'read_key')
+    write_key = credentials_config.get('apis', 'write_key')
 
     while(True):
-        process_pending_hosts(read_url, write_url)
+        process_pending_hosts(read_url, read_key, write_url, write_key)
         time.sleep(sleep_time)
 
 if __name__ == '__main__':
